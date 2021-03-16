e12c320eb151ff21186475fa78bc628304b88f89
Saadanwar87 <dr.lababidi87@gmail.com>
2021-03-16 02:41:30 -0400

    deleted unnecessary files and converted pricing service to microservice.

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (id, currency, price) VALUES (1, 'USD', 35000.58);
+INSERT INTO price (id, currency, price) VALUES (2, 'USD', 25000.33);
+INSERT INTO price (id, currency, price) VALUES (3, 'USD', 20000.21);
+INSERT INTO price (id, currency, price) VALUES (4, 'USD', 16000.09);
+INSERT INTO price (id, currency, price) VALUES (5, 'USD', 45000.98);
