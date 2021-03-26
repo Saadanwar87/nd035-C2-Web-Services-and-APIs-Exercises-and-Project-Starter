9a9cf42e34eda095503bc3f19e13a5182f34f046
Saadanwar87 <dr.lababidi87@gmail.com>
2021-03-26 00:10:18 -0400

    Implemented Todos in CarControllerTest class

--- P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -22,11 +22,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.net.URI;
+import java.nio.charset.Charset;
 import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -91,6 +93,13 @@ public class CarControllerTest {
          *   the whole list of vehicles. This should utilize the car from `getCar()`
          *   below (the vehicle will be the first in the list).
          */
+       Car car = getCar();
+       MediaType content = new MediaType("application", "hal+json", Charset.forName("UTF-8"));
+       mvc.perform(
+                    get("/cars"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(content))
+                    .andExpect(content().json("{\"_embedded\":{},\"_links\":{\"self\":{\"href\":\"http://localhost/cars\"}}}"));
 
     }
 
@@ -104,6 +113,12 @@ public class CarControllerTest {
          * TODO: Add a test to check that the `get` method works by calling
          *   a vehicle by ID. This should utilize the car from `getCar()` below.
          */
+        Car car = getCar();
+        MediaType content = new MediaType("application", "hal+json", Charset.forName("UTF-8"));
+        mvc.perform(
+                get("/cars/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(content));
     }
 
     /**
@@ -117,6 +132,10 @@ public class CarControllerTest {
          *   when the `delete` method is called from the Car Controller. This
          *   should utilize the car from `getCar()` below.
          */
+        Car car = getCar();
+        mvc.perform(delete("/cars/1"))
+                .andExpect(status().is(204))
+                .andExpect(status().isNoContent());
     }
 
     /**
